304510dda8339d86f5778b88a027b54c526bd448
LuisContrerasGlz <luis.fco.contreras@gmail.com>
2022-12-16 13:57:45 -0600

    Adding readme file

--- exceptions.py
+++ exceptions.py
@@ -5,3 +5,15 @@ try:
 except: 
     print("The index does not exist in the list.")
 
+# ZeroDivisionError
+
+def divide_by(a, b):
+    try:
+        return a / b
+    except ZeroDivisionError:
+        return 0
+    except Exception as e:
+        print(e, 'Something went wrong!')
+
+ans = divide_by(10,0)
+print(ans)
