46999b29773cd455e3424b59a51aa84f8cb427be
LuisContrerasGlz <luis.fco.contreras@gmail.com>
2022-12-18 14:04:26 -0600

    progress

--- OOP/Inheritance.py
+++ OOP/Inheritance.py
@@ -6,3 +6,25 @@ class Child(Parent):
     Inherited members from parent class
     Additional members of the child class
 """
+
+class House:
+    '''
+    This is a stub for a class representing a house that can be used to create objects and evaluate different metrics that we may require in constructing it.
+    '''
+    num_rooms = 5
+    bathrooms = 2
+
+    def cost_evaluation(self, rate):
+        # Functionality to calculate the costs from the area of the house
+        cost = rate * self.num_rooms
+        return cost
+
+
+
+house = House()
+print(house.num_rooms)
+print(House.num_rooms)
+house.num_rooms = 7
+# House.num_rooms = 7
+print(house.num_rooms)
+print(House.num_rooms)
